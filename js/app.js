aa3046dc436e856498d437c823239b50a727cd25
bernardoteixeira2008 <bernardotellesdosantos@gmail.com>
2026-09-06 20:52:11 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -272,7 +272,14 @@
 
     let html = `<div class="sabores-grid">`;
     produto.sabores.forEach((sabor) => {
-      html += `<button type="button" class="sabor-item" data-sabor="${sabor}">${sabor}</button>`;
+      const thumb = sabor.imagem
+        ? `<span class="sabor-thumb" style="background-image:url('assets/${sabor.imagem}')"></span>`
+        : `<span class="sabor-thumb sabor-thumb-vazio">🍨</span>`;
+      html += `
+        <button type="button" class="sabor-item" data-sabor="${sabor.nome}">
+          ${thumb}
+          <span class="sabor-nome">${sabor.nome}</span>
+        </button>`;
     });
     html += `</div>`;
     $("#modalCorpoDinamico").innerHTML = html;
